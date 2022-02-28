3898b35ca0bb3333493bd26a2a598b2cd79cd135
Michael Dickens <mdickens93@gmail.com>
2022-02-28 16:45:55 -0500

    Working on improving accuracy of numerical integration

--- MissionHedging.hs
+++ MissionHedging.hs
@@ -58,8 +58,17 @@ multiNormRVs zScores alphas covars =
   alphas covars
 
 
+-- | Multivariate lognormal PDF
+--
+-- Same as a normal PDF but where input vector `ys` is transformed by taking the
+-- logarithm, and whole thing is multiplied by `det(Jacobian(ys))` = `1 /
+-- product ys` to adjust the base area of the Riemann sum.
 -- https://stats.stackexchange.com/questions/214997/multivariate-log-normal-probabiltiy-density-function-pdf
 -- https://math.stackexchange.com/questions/267267/intuitive-proof-of-multivariable-changing-of-variables-formula-jacobian-withou
+--
+-- ys: n-dimensional random variable
+-- mus: means of the underlying normal distributions
+-- covars: covariance matrix for the underlying normal distributions
 lognormPDF :: [Double] -> [Double] -> [[Double]] -> Double
 lognormPDF ys mus covars =
   exp ((-0.5) * ((residuals <# inv covMat) `dot` residuals))
@@ -75,7 +84,7 @@ uniLognormPDF' y mu cov = (1 / sqrt (2*pi * cov)) * (1 / y) * exp ((-0.5) * (log
 
 
 -- With 6 stdev, the Riemann sum is a slight overestimate. With 5 stdev, it's a slight underestimate
-integralMaxStdev = 6 :: Double
+integralMaxStdev = 5 :: Double
 integralNumDivisions = 12 :: Int
 integralTrapezoidWidth = integralMaxStdev / fromIntegral integralNumDivisions
 
@@ -93,7 +102,7 @@ integralRanges = [[x, y, z] | x <- uniRanges, y <- uniRanges, z <- uniRanges]
 
 modelDistribution :: ([Double], [[Double]])
 modelDistribution =
-  let alphas = [0.08, 0.04, 0.03] :: [Double]
+  let alphas = [0.08, 0.00, 0.05] :: [Double]
       sigmas = diagl [0.18, 0.18, 0.02] :: Matrix Double
       hedgeCorrelation = 0.5
       correlations = (3><3)
@@ -107,12 +116,29 @@ modelDistribution =
 
 -- | Utility function with constant relative risk aversion.
 crraUtility :: Double -> Double -> Double
-crraUtility 1 money = log money
-crraUtility rra money = (money**(1 - rra) - 1) / (1 - rra)
+crraUtility 1 wealth = log wealth
+crraUtility rra wealth = (wealth**(1 - rra) - 1) / (1 - rra)
 
 
+-- | For rra > 1, this utility function has the following properties:
+--
+-- dU/dw > 0                  (utility increases with wealth)
+-- dU/db < 0                  (utility decreases with bad thing)
+-- d^2 U / dw^2 < 0           (diminishing marginal utility of wealth)
+-- d^2 U / dw db > 0          (wealth becomes more useful as bad thing increases)
+-- lim(w -> inf) U(w, b) = 0  (infinite wealth fully cancels out bad thing)
+multiCrraUtility :: Double -> Double -> Double -> Double
+multiCrraUtility rra wealth bad = bad * (wealth**(1 - rra)) / (1 - rra)
+
+
+ostensiblyIrrelevantOffset = 0
+
+
+-- TODO: optimal allocation changes wrt k, but it shouldn't
 utility :: Double -> Double -> Double
-utility wealth co2 = log wealth -- * co2
+-- utility = multiCrraUtility rra
+utility wealth bad = (wealth**(1 - rra) - ostensiblyIrrelevantOffset) / (1 - rra)
+  where rra = 1.1
 
 
 expectedUtility :: [Double] -> Double
@@ -120,31 +146,56 @@ expectedUtility weights' =
   sum $ map (
   \rectRanges ->
     let (alphas', covars') = modelDistribution
-        weights = weights' ++ [1]  -- dummy weight for co2
+        weights = weights' ++ [1]  -- dummy weight for badThing
 
         alphas = zipWith (*) weights alphas'
         covars = [[weights!!i * weights!!j * covars'!!i!!j | i <- [0..2]] | j <- [0..2]]
 
-        mus = zipWith (\alpha row -> alpha - 0.5 * sum row) alphas covars
         stdevs = [sqrt $ covars!!i!!i | i <- [0..2]]
-        transform point = zipWith4 (\mu sd wt x -> exp $ mu + sd * sqrt (abs wt) * signum wt * x) mus stdevs weights point
+        mus = zipWith (\alpha sd -> alpha - 0.5 * sd**2) alphas stdevs
 
-        y0 = transform $ map fst rectRanges
-        y1 = transform $ map snd rectRanges
-        width = product $ zipWith (-) y1 y0
-        pdf = lognormPDF (transform $ map (\(e, s) -> (e + s) / 2) rectRanges) mus covars
+        -- | Transform from standard normal space to nonstandard lognormal space
+        transform point = zipWith3 (\mu sd x -> exp $ mu + sd * x) mus stdevs point
+        -- transform point = zipWith3 (\mu row x ->
+        --                               let v = (sum $ map (* x) row)
+        --                               in exp $ mu + sqrt (abs v) * signum v) mus covars point
 
-        -- wealth = exp $ sum $ take 2 xs
-        -- co2 = ys!!2
-        -- u = utility wealth co2
-    -- in u * pdf * width
-    in pdf * width
+        yLower = transform $ map fst rectRanges
+        yUpper = transform $ map snd rectRanges
+        ys = transform $ map (\(e, s) -> (e + s) / 2) rectRanges
+        width = abs $ product $ zipWith (-) yUpper yLower
+        pdf = lognormPDF ys mus covars
+
+        -- Recall that ys are already weighted by portfolio allocation. Per
+        -- Irlam, you can think of this as holding y[0] for a proportionate
+        -- length of time, then holding y[1].
+        wealth = product $ take 2 ys
+
+        badThing = ys!!2
+        u = utility wealth badThing
+    in u * pdf * width
   ) integralRanges
 
 
-gradientDescentIO :: ([Double] -> Double) -> [Double] -> Double -> Int -> IO ([Double])
-gradientDescentIO f initialGuess scale numSteps = go initialGuess numSteps
-  where go :: [Double] -> Int -> IO ([Double])
+getGradient :: ([Double] -> Double) -> [Double] -> [Double]
+getGradient f x = map (\i -> (f (updateAt i (+h) x) - fx) / h) [0..(length x - 1)]
+  where fx = f x
+        h = 0.0001
+
+
+-- | Perform gradient ascent, printing the results at each step.
+--
+-- - f :: [Double] -> Double: The function to be maximized.
+-- - initialGuess :: [Double]: The point at which to begin gradient ascent.
+-- - scale :: [Double]: A parameter that determines the speed of ascent for each
+--   parameter. A larger value means it will converge faster, but too large of a
+--   scale means it will fail to converge.
+--
+-- Gradient ascent halts if it does not converge after a certain number of steps.
+gradientAscentIO :: ([Double] -> Double) -> [Double] -> [Double] -> IO ([Double])
+gradientAscentIO f initialGuess scale = go initialGuess numSteps
+  where numSteps = 100
+        go :: [Double] -> Int -> IO ([Double])
         go x 0 = do
           printInfo x (f x)
           printf " (failed to converge after %d steps)\n" numSteps
@@ -154,24 +205,47 @@ gradientDescentIO f initialGuess scale numSteps = go initialGuess numSteps
               fx = f x
               gradient = map (\i -> (f (updateAt i (+h) x) - fx) / h) [0..(length x - 1)]
               distance = sqrt $ sum $ map (**2) gradient
-          in if isNaN distance || distance < 0.000001
+          in if isNaN distance || distance < 0.00001
              then do
                printInfo x fx
                printf " (converged after %d steps)\n" (numSteps - stepsLeft + 1)
                return x
              else do
                printInfo x fx
-               printf " (step %d)\r" (numSteps - stepsLeft + 1)
-               go (zipWith (+) x $ map (* scale) gradient) (stepsLeft - 1)
+               printf " (step %d) -> [%.5f, %.5f]\r" (numSteps - stepsLeft + 1) (gradient!!0) (gradient!!1)
+               go (zipWith3 (\xi s g -> xi + s * g) x scale gradient) (stepsLeft - 1)
 
-        printInfo x fx = printf "EU(%.4f, %.4f) = %.4f" (x!!0) (x!!1) fx
+        printInfo x fx = printf "EU(%.3f, %.3f) = %.4f" (x!!0) (x!!1) fx
 
 
+-- | Approximate geometric mean of an investment portfolio.
+--
+-- From Estrada (2010), "Geometric Mean Maximization: An Overlooked Portfolio Approach?"
+-- https://blog.iese.edu/jestrada/files/2012/06/GMM-Extended.pdf
+--
+-- TODO: For some reason, the naive (alpha - cov/2) formula is a much better
+-- approximation, especially close to the GMM-maximizing weights.
+geometricMean :: [Double] -> [[Double]] -> [Double] -> Double
+geometricMean alphas covars weights =
+  -- log (1 + weightVec `dot` alphaVec)
+  -- - 0.5 * ((weightVec <# covMat) `dot` weightVec) / (1 + weightVec `dot` alphaVec)**2
+  weightVec `dot` alphaVec - 0.5 * ((weightVec <# covMat) `dot` weightVec)
+  where alphaVec = Vec.fromList $ take 2 alphas
+        covMat = takeRows 2 $ takeColumns 2 $ fromLists covars
+        varVec = takeDiag covMat
+        weightVec = Vec.fromList $ take 2 weights
+        muVec = Vec.zipWith (\a v -> a - v**2 / 2) alphaVec varVec
+
+
+-- TODO: The problem is that integration isn't very accurate when weights are small, eg with hedge weight = 0.01, integral only covers 45% of the probability space
 main :: IO ()
 main = do
   let (alphas, covars) = modelDistribution
 
-  print $ expectedUtility [0.5, 0.5]
-  putStrLn ""
-  -- res <- gradientDescentIO expectedUtility ([0.5, 0.5]) 10 100
+  let printEU x = printf "EU(%.3f, %.3f) = %.4f\n" (x!!0) (x!!1) (expectedUtility x)
+  print $ (10 - 10*ostensiblyIrrelevantOffset) + expectedUtility [1.878, -0.012]
+  print $ getGradient expectedUtility [1.878, -0.012]
+  weights <- gradientAscentIO expectedUtility ([1.5, 0.25]) [5, 5]
+  -- sequence $ map (\hedge -> printf "%.4f: %.5f\n" (hedge::Double) (expectedUtility [1.557, hedge])) $ filter (/= 0) $ map ((/ 10000) . fromIntegral) [(-50 :: Int)..50]
+  -- weights <- gradientAscentIO expectedUtility ([0.5, -1]) [5, 5]
   return ()
